e85476c5cc019eb91c1104dbdc08aaaedf496d87
Byron Garcia <82432096+DevBGarcia@users.noreply.github.com>
2024-06-05 15:59:45 -0700

    CHECKPOINT -> Need to figure out how to fix typescript error with navigator

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { IconProps } from 'react-native-vector-icons/Icon';
 
 export type IconButtonProps = {
--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Text, Alert, AppState, AppStateStatus } from 'react-native';
+import { Text, Alert, AppState, AppStateStatus, View, StyleSheet } from 'react-native';
 import BackgroundTimer from 'react-native-background-timer';
 import PushNotification from 'react-native-push-notification';
 import { GlobalStyles } from '../styles/GlobalStyles';
 import { formatTime } from '../utils/FormatTime';
 import IconButton from './IconButton';
+import { useNavigation } from '@react-navigation/native';
+import { TIMERS_NAVIGATOR_SCREEN_NAMES } from '../navigators/TimersNavigator';
+
 
 type TimerProps = {
   initialSeconds?: number;
@@ -15,6 +18,7 @@ export const Timer = ({ initialSeconds = 5 }: TimerProps) => {
   const [isActive, setIsActive] = useState(false);
   const [appState, setAppState] = useState(AppState.currentState);
   const intervalId = useRef<number | null>(null);
+  const navigation = useNavigation();
 
   console.log('BG - seconds', seconds);
 
@@ -22,13 +26,13 @@ export const Timer = ({ initialSeconds = 5 }: TimerProps) => {
     const appStateListener = AppState.addEventListener('change', handleAppStateChange);
 
     return () => {
-        appStateListener.remove();
+      appStateListener.remove();
     };
   }, []);
 
   useEffect(() => {
     if (isActive && seconds > 0) {
-        intervalId.current = BackgroundTimer.setInterval(() => {
+      intervalId.current = BackgroundTimer.setInterval(() => {
         setSeconds((currentSeconds) => {
           if (currentSeconds === 1) {
             BackgroundTimer.clearInterval(intervalId.current!);
@@ -56,7 +60,7 @@ export const Timer = ({ initialSeconds = 5 }: TimerProps) => {
     }
 
     return () => BackgroundTimer.clearInterval(intervalId.current!);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isActive, seconds, appState]);
 
   const handleAppStateChange = (nextAppState: AppStateStatus) => {
@@ -68,26 +72,44 @@ export const Timer = ({ initialSeconds = 5 }: TimerProps) => {
     setIsActive(false);
   };
 
-return (
+  const styles = StyleSheet.create({
+    timerActionButtonsContainer: {
+      flexDirection: 'row',
+      gap: 8,
+      paddingVertical: 16,
+    },
+  });
+
+  return (
     <>
-        <Text style={GlobalStyles.h1}>{formatTime(seconds)}</Text>
-         <IconButton
-            isDisabled={seconds <= 0}
-            IconButtonIconProps={{
-                    name: isActive ? 'pause-circle-outline' : 'play-circle-outline',
-            }}
-            IconButtonTouchableOpacityProps={{
-                onPress: () => setIsActive((prev) => !prev),
-            }}
-         />
-         <IconButton
-            IconButtonIconProps={{
-                    name: 'reload-circle-outline',
-            }}
-            IconButtonTouchableOpacityProps={{
-                onPress: reset,
-            }}
-         />
+      <Text style={GlobalStyles.h1}>{formatTime(seconds)}</Text>
+      <View style={styles.timerActionButtonsContainer}>
+        <IconButton
+          IconButtonIconProps={{
+            name: 'reload',
+          }}
+          IconButtonTouchableOpacityProps={{
+            onPress: reset,
+          }}
+        />
+        <IconButton
+          isDisabled={seconds <= 0}
+          IconButtonIconProps={{
+            name: isActive ? 'pause-circle-outline' : 'play-circle-outline',
+          }}
+          IconButtonTouchableOpacityProps={{
+            onPress: () => setIsActive((prev) => !prev),
+          }}
+        />
+        <IconButton
+          IconButtonIconProps={{
+            name: 'pencil-circle-outline',
+          }}
+          IconButtonTouchableOpacityProps={{
+            onPress: () => navigation.navigate(TIMERS_NAVIGATOR_SCREEN_NAMES.EditTimer),
+          }}
+        />
+      </View>
     </>
-);
+  );
 };
