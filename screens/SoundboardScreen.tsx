455fa7e153a85500f388e7a69258a68c22460e24
DevBGarcia <DevBGarcia@gmail.com>
2024-07-19 16:54:13 -0700

    Init sound board basic implementation

--- screens/SoundboardScreen.tsx
+++ screens/SoundboardScreen.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { GlobalStyles } from '../styles/GlobalStyles';
+import Sound from 'react-native-sound';
+import Round1Audio from '../audio/Round01.mp3';
 
 export const SoundboardScreen = () => {
+  // Enable playback in silence mode
+  Sound.setCategory('Playback');
+
+  const playSound = () => {
+    // Initialize the sound
+    var sound = new Sound(Round1Audio, Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.log('Failed to load the sound', error);
+        return;
+      }
+      // Play the sound if loading was successful
+      sound.play((success) => {
+        if (!success) {
+          console.log('Playback failed due to audio decoding errors');
+        }
+      });
+    });
+  };
+
   return (
     <View style={GlobalStyles.screenContainer}>
-      <Text>Soundboard Screen</Text>
+      <Text>Timer List Screen</Text>
+      <Button
+        title="Play Sound"
+        onPress={playSound}
+      />
     </View>
   );
 };
